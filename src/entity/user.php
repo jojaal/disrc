f8b6ab46eb6b5ac4bf89750846484a2d2ef41050
android <android@localhost.localdomain>
2014-07-31 16:12:25 +0200

    user review

--- src/entity/user.php
+++ src/entity/user.php
@@ -6,13 +6,15 @@ namespace Entity;
   convertir l'objet en chaine de charactères où créer un objet de type User
   depuis une chaine de charactère contenant les attributs de la classe.
   */
-class User implements \Serializable {
+class User implements \Serializable
+{
   private $nom;
   private $prenom;
   private $age;
 
   // Le constructeur de User prend en paramètre un nom, prénom, age.
-  function __construct($nom, $prenom, $age) {
+  function __construct($nom, $prenom, $age)
+  {
     $this->nom = $nom;
     $this->prenom = $prenom;
     $this->age = $age;
@@ -25,12 +27,14 @@ class User implements \Serializable {
     */
 
   // On indique quels attributs de la classes seront à sérialiser.
-  public function serialize() {
+  public function serialize()
+  {
     return serialize(array($this->nom, $this->prenom, $this->age));
   }
 
   // On indique de quelle manière la classe sera désérilisable.
-  public function unserialize($data) {
+  public function unserialize($data)
+  {
     $attr = unserialize($data);
     $this->nom = $attr[0];
     $this->prenom = $attr[1];
@@ -38,27 +42,33 @@ class User implements \Serializable {
   }
 
   // Ci dessous, les différents accesseurs pour les attributs de la classe.
-  public function getNom() {
+  public function getNom()
+  {
     return $this->nom;
   }
 
-  public function getPrenom() {
+  public function getPrenom()
+  {
     return $this->prenom;
   }
 
-  public function getAge() {
+  public function getAge()
+  {
     return $this->age;
   }
 
-  public function setNom($nom) {
+  public function setNom($nom)
+  {
     $this->nom = $nom;
   }
 
-  public function setPrenom($prenom) {
+  public function setPrenom($prenom)
+  {
     $this->prenom = $prenom;
   }
 
-  public function setAge($age) {
+  public function setAge($age)
+  {
     $this->age = $age;
   }
 }
